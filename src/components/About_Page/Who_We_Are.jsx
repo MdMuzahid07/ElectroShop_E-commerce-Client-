a30aab94b33e18bb28ff6479ce742707cc33c6c2
MdMuzahid07 <MdMuzahid7396@gamil.com>
2022-08-03 03:45:41 +0600

    about UI almost complete

---
+++ src/components/About_Page/Who_We_Are.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Who_We_Are = () => {
+    return (
+        <section className='grid md:grid-cols-2 gap-7 my-20'>
+            <div>
+                <iframe className='w-full h-96' src="https://www.youtube.com/embed/wf4F2-9UXUo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div>
+                <h2 className='text-3xl font-bold'><span className='text-primary'>|</span> Who we are?</h2>
+                <p className='mt-5'>
+                    We believe in a world where you have total freedom to be you, without judgement. To experiment. To express yourself. To be brave and grab life as the extraordinary adventure it is. So we make sure everyone has an equal chance to discover all the amazing things they’re capable of – no matter who they are, where they’re from or what looks they like to boss.
+                    <br />
+                    <br />
+                    Our audience (AKA you) is wonderfully unique. And we do everything we can to help you find your fit, offering our Ciloe Brands in more than 30 sizes – and we’re committed to providing all sizes at the same price</p>
+            </div>
+        </section>
+    );
+};
+
+export default Who_We_Are;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Our_Team from '../components/About_Page/Our_Team';
 import Page_Thumbnail from '../components/About_Page/Page_Thumbnail';
+import Who_We_Are from '../components/About_Page/Who_We_Are';
+import Brand_we_work_with from '../components/Home_Page/Brand_we_work_with';
 
 const About = () => {
     return (
         <div className='my-7'>
             <Page_Thumbnail />
+            <Who_We_Are />
+            <Our_Team />
+            <Brand_we_work_with />
         </div>
     );
 };
