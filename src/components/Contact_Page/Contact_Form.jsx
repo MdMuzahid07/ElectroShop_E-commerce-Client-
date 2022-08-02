96673f1a26995564d297a1e170a46c4be2929846
MdMuzahid07 <MdMuzahid7396@gamil.com>
2022-08-03 03:59:18 +0600

    react hooks form install and working on it

---
+++ src/components/Contact_Page/Contact_Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact_Form = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Contact_Form;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contact_Form from '../components/Contact_Page/Contact_Form';
 import Google_Map from '../components/Contact_Page/Google_Map';
 import Page_Thumbnail from '../components/Contact_Page/Page_Thumbnail';
 
@@ -7,6 +8,7 @@ const Contact = () => {
         <div className='my-7'>
             <Page_Thumbnail />
             <Google_Map />
+            <Contact_Form />
         </div>
     );
 };
