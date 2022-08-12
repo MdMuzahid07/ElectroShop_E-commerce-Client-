5fdf6c9142d8009596985545899898687765e8b9
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-12 16:59:59 +0600

    working on manage product page on dashboard

--- src/Dashboard/AdminPages/ManageProducts.jsx
+++ src/Dashboard/AdminPages/ManageProducts.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const ManageProducts = () => {
     return (
-        <div>
-            <h1>manage products</h1>
+        <div className='my-7'>
         </div>
     );
 };
