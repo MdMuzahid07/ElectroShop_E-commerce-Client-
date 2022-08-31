17b437821987853660099c255e85ee714a0ff814
MdMuzahid07 <MdMuzahid7396@gamil.com>
2022-08-31 12:20:26 +0600

    Click top page top button UI changed

--- src/components/Vanilla_back_to_top/BackToTopButton.jsx
+++ src/components/Vanilla_back_to_top/BackToTopButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BackToTopButton = () => {
     return (
         <div className='absolute bottom-28 right-5'>
-            <button className="btn btn-circle btn-lg btn-outline btn-accent lg:btn-primary z-20">
+            <button className="btn btn-circle btn-lg btn-outline btn-accent z-20">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none" viewBox="0 0 24 24"
